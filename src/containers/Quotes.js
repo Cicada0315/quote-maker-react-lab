cac96f14a3e6c0f792ab1246b19fb37ed8fb202e
jean <jin.ahn.1027@gmail.com>
2021-06-12 18:16:59 -0400

    commit tree 3fbe122a59099efec43f813e49419381649608b2

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes'
 
 class Quotes extends Component {
 
   render() {
+    let quotes= this.props.quotes.map(quote => <li key={quote.id}><QuoteCard quote={quote} upvoteQuote={this.props.upvoteQuote} downvoteQuote={this.props.downvoteQuote} removeQuote={this.props.removeQuote}/></li>);
     return (
       <div>
         <hr />
@@ -20,6 +22,7 @@ class Quotes extends Component {
 
                 Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
                */}
+               {quotes}
             </div>
           </div>
         </div>
@@ -28,5 +31,11 @@ class Quotes extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    quotes: state.quotes
+  }
+}
+
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(mapStateToProps, { removeQuote, upvoteQuote, downvoteQuote })(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,32 @@
 export default (state = [], action) => {
-  return state;
+  let index;
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId);
+    case "UPVOTE_QUOTE":
+      index=state.findIndex(quote => {return quote.id===action.quoteId} );
+      if (index > -1) {
+        let upQuote = Object.assign({}, state[index], { votes: state[index].votes + 1 });
+        return [...state.slice(0, index), upQuote, ...state.slice(index + 1)];
+      }else{
+        return state
+      }
+
+    case "DOWNVOTE_QUOTE":
+      index=state.findIndex(quote => {return quote.id===action.quoteId} );
+      if (index > -1) {
+        let downQuote = Object.assign({}, state[index], { votes: state[index].votes - 1 });
+        if (downQuote.votes < 0 ){
+          downQuote.votes = 0;
+        } 
+        return [...state.slice(0, index), downQuote, ...state.slice(index + 1)];
+      }else{
+        return state;
+      }
+    
+    default:
+      return state;
+  }
 }
