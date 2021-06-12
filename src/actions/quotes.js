f2c30077d20422129e4cd2b46879ae2293fab151
jean <jin.ahn.1027@gmail.com>
2021-06-12 18:17:09 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,28 @@
 // TODO: Create action creators as defined in tests
+export const addQuote = (quote={}) => {
+    return { 
+        type: 'ADD_QUOTE', 
+        quote
+    };
+};
+
+export const removeQuote = (quoteId= null) => {
+    return { 
+        type: 'REMOVE_QUOTE', 
+        quoteId
+    };
+};
+
+export const upvoteQuote = (quoteId= null) => {
+    return { 
+        type: 'UPVOTE_QUOTE', 
+        quoteId
+    };
+};
+
+export const downvoteQuote = (quoteId= null) => {
+    return { 
+        type: 'DOWNVOTE_QUOTE', 
+        quoteId
+    };
+};
